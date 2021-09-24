3b706dc15f9a983f550ba9bd485d5687e3c3cba2
Ahmed B <techiediaries9@gmail.com>
2021-09-24 18:23:19 +0100

    Create a simple GraphQL API

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,15 +1,37 @@
-// Our code is being watched and recompiled now thanks to ts-node-dev
- 
-import express, { Application } from 'express'; 
+import express, { Application } from 'express';
+import { ApolloServer, Config, gql } from 'apollo-server-express';
+import { IResolvers } from '@graphql-tools/utils';
 
-const PORT = 8080; 
+const typeDefs = gql` 
+    type Query { 
+        message: String! 
+    } 
+`
+const resolvers: IResolvers = {
+  Query: {
+    message: () => 'It works!'
+  }
+};
 
-const app: Application = express(); 
+const config: Config = {
+  typeDefs: typeDefs,
+  resolvers: resolvers
+};
 
-app.get('/', (req, res) => res.send('Express is successfully running!'));  
+async function startApolloServer(config: Config) {
+  const PORT = 8080;
+  const app: Application = express();
+  const server: ApolloServer = new ApolloServer(config);
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: '/graphql'
+  });
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
 
-app.listen(PORT, () => { 
+startApolloServer(config);
 
-  console.log(`Server is running at http://localhost:${PORT}`); 
 
-}); 
