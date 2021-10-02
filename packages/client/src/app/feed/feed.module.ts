dfdde9bf0f6c0e32028b1c5fc81f1c29ce15d07c
Ahmed B <techiediaries9@gmail.com>
2021-10-03 00:12:52 +0100

    Generate feed's posts component

--- packages/client/src/app/feed/feed.module.ts
+++ packages/client/src/app/feed/feed.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeedRoutingModule } from './feed-routing.module';
+import { PostsComponent } from './components/posts/posts.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PostsComponent
+  ],
   imports: [
     CommonModule,
     FeedRoutingModule
