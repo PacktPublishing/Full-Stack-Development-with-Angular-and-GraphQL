d6858f215a2b64b69bf450b64ca078664bde1639
Ahmed B <techiediaries9@gmail.com>
2021-10-02 23:54:24 +0100

    Generate core components & auth service

--- packages/client/src/app/app.module.ts
+++ packages/client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { SearchDialogComponent } from './core/components/search-dialog/search-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    SearchDialogComponent
   ],
   imports: [
     BrowserModule,
---
+++ packages/client/src/app/core/components/search-dialog/search-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-dialog',
+  templateUrl: './search-dialog.component.html',
+  styleUrls: ['./search-dialog.component.css']
+})
+export class SearchDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
