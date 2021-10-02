cfcbd118abf43fb20d30a65c277df2cbb5b5b9c5
Ahmed B <techiediaries9@gmail.com>
2021-10-02 23:48:04 +0100

    Import ReactiveFormsModule in shared module & re-export it

--- packages/client/src/app/shared/shared.module.ts
+++ packages/client/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule, ReactiveFormsModule
+  ],
+  exports: [ReactiveFormsModule]
 })
 export class SharedModule { }
