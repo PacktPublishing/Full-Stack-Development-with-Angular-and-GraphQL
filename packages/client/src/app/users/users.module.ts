07af89026f579cf439d90182c66cf718d427c1b0
Ahmed B <techiediaries9@gmail.com>
2021-10-02 23:57:57 +0100

    Generate users' module components

--- packages/client/src/app/users/users.module.ts
+++ packages/client/src/app/users/users.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent
+  ],
   imports: [
     CommonModule,
     UsersRoutingModule
