616b514c5c0c7738e62fdd65c0e8a3c6c9cb9ff8
Ahmed B <techiediaries9@gmail.com>
2021-10-02 23:38:18 +0100

    Generate Angular modules

---
+++ packages/client/src/app/feed/feed.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeedRoutingModule } from './feed-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeedRoutingModule
+  ]
+})
+export class FeedModule { }
