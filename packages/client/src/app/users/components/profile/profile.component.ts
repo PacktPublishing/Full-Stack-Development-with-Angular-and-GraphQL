104e9acf07918305d8a8039bc12077f06164ae2f
Ahmed B <techiediaries9@gmail.com>
2021-10-03 01:29:58 +0100

    Get userId from /profile route

--- packages/client/src/app/users/components/profile/profile.component.ts
+++ packages/client/src/app/users/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      console.log(params['userId']);
+    });
   }
 
 }
