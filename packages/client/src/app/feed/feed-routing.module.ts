15556a614a675250048f66f928770b2e18cd559a
Ahmed B <techiediaries9@gmail.com>
2021-10-03 01:03:18 +0100

    Implement routing

--- packages/client/src/app/feed/feed-routing.module.ts
+++ packages/client/src/app/feed/feed-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'posts' },
+  { path: 'posts', component: PostsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- packages/client/src/app/users/users-routing.module.ts
+++ packages/client/src/app/users/users-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'profile/:userId', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
