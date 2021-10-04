f13c9f42a01893d666df03091558480eddfaeb9d
Ahmed B <techiediaries9@gmail.com>
2021-10-04 12:15:53 +0100

    Implement header component

--- packages/client/src/app/core/components/header/header.component.ts
+++ packages/client/src/app/core/components/header/header.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { User, SearchUsersResponse } from 'src/app/shared';
+import { AuthService } from 'src/app/core';
+import { SearchDialogComponent } from '../search-dialog/search-dialog.component';
 
 @Component({
   selector: 'app-header',
@@ -6,13 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('searchInput') searchInput: ElementRef | null = null;
+  fetchMore: (users: User[]) => void = (users: User[]) => { };
+  constructor(
+    public authService: AuthService,
+    public matDialog: MatDialog,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
-  invokeMe() {
-    console.log("Invoked from the console");
+  logOut(): void {
+    this.authService.logOut();
+    this.router.navigateByUrl('/users/login');
+  }
+  searchUsers(): void {
+    const searchText = this.searchInput?.nativeElement.value;
+    const result: SearchUsersResponse = this.authService.searchUsers(searchText, 0, 10);
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = result;
+    this.matDialog.open(SearchDialogComponent, dialogConfig);
   }
 
 }
+
