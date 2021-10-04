ab156dc1736530f1672d6615abd06637c911ea29
Ahmed B <techiediaries9@gmail.com>
2021-10-04 22:38:14 +0100

    Refactor GQL documents with fragments

--- packages/client/src/app/shared/constants/user.ts
+++ packages/client/src/app/shared/constants/user.ts
@@ -1,15 +1,18 @@
 import { gql } from 'apollo-angular';
+import {
+  USER_FIELDS_FRAGMENT,
+  BASIC_USER_FIELDS_FRAGMENT
+} from './user.fragments';
 
 export const USER_QUERY = gql`
+ ${USER_FIELDS_FRAGMENT}
  query getUser($userId: ID!){  
-  getUser(userId:$userId){
-   id fullName bio email username image coverImage postsCount createdAt }
+  getUser(userId:$userId){ ...UserFields }
  } 
-`; 
+`;
 export const SEARCH_USERS_QUERY = gql` 
+ ${BASIC_USER_FIELDS_FRAGMENT}
  query searchUsers($searchQuery: String!){  
-  searchUsers(searchQuery:$searchQuery){         
-   id fullName bio username image  
-  }  
+  searchUsers(searchQuery:$searchQuery){ ...BasicUserFields }  
  }  
-`; 
+`;
