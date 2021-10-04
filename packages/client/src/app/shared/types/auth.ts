dab8a61fab5fb723535615ea7ed0e64a72fc3e97
Ahmed B <techiediaries9@gmail.com>
2021-10-04 11:35:37 +0100

    Add auth types to shared

---
+++ packages/client/src/app/shared/types/auth.ts
@@ -0,0 +1,17 @@
+import { User } from '../models/user.model';
+
+export interface AuthState {
+  isLoggedIn: boolean;
+  currentUser: User | null;
+  accessToken: string | null;
+};
+export type AuthResponse = {
+  token: string;
+  user: User;
+};
+export type RegisterResponse = {
+  register: AuthResponse;
+};
+export type LoginResponse = {
+  signIn: AuthResponse;
+};
