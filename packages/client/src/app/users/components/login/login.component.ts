3d453ae5a4ca8113a8c0c2990223cc7bc22f3fa6
Ahmed B <techiediaries9@gmail.com>
2021-10-04 12:12:28 +0100

    [Assign] Build & style login UI

--- packages/client/src/app/users/components/login/login.component.ts
+++ packages/client/src/app/users/components/login/login.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from 'src/app/core';
+import { LoginResponse } from 'src/app/shared';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+  });
 
-  constructor() { }
+  errorMessage: string = '';
+  private loginSubscription: Subscription | null = null;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService,
+    private snackBar: MatSnackBar,
+    private router: Router) { }
+
+  ngOnInit(): void {}
+  ngOnDestroy() {
+    if (this.loginSubscription) {
+      this.loginSubscription.unsubscribe();
+    }
+  }  
+  submit() {
+    const { email, password } = this.form.value;
+    if (!this.form.valid) {
+      this.errorMessage = 'Please enter valid email and password';
+      return;
+    }
+    this.loginSubscription = this.authService.login(email, password).subscribe({
+      next: (result: LoginResponse | null | undefined) => {
+        if (result) {
+          const savedUserId = result.signIn.user?.id;
+          this.snackBar.open('Login Success!', 'Ok', {
+            duration: 5 * 1000
+          });
+          this.router.navigateByUrl(`/users/profile/${savedUserId}`);
+        }
+      },
+      error: (err) => {
+        console.error(err.message);
+        this.snackBar.open(err.message, 'Ok', {
+          duration: 5 * 1000
+        });
+      }
+    });
   }
-
 }
