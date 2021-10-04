e89a61f69cf32942a3b249cb72ad9e1aadd1c9c6
Ahmed B <techiediaries9@gmail.com>
2021-10-04 11:22:59 +0100

    Import & reexport Material components from shared

--- packages/client/src/app/shared/shared.module.ts
+++ packages/client/src/app/shared/shared.module.ts
@@ -1,13 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatBadgeModule } from '@angular/material/badge';
 
+const matModules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatTooltipModule,
+  MatSnackBarModule,
+  MatDialogModule,
+  MatProgressBarModule,
+  MatBadgeModule
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, ReactiveFormsModule
+    CommonModule, ReactiveFormsModule, ...matModules
   ],
-  exports: [ReactiveFormsModule]
+  exports: [ReactiveFormsModule, ...matModules]
 })
 export class SharedModule { }
