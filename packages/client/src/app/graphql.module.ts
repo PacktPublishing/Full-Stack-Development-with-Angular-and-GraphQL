196f354188e2008d19e898cbdc6e948358f0521b
Ahmed B <techiediaries9@gmail.com>
2021-10-04 18:05:46 +0100

    Send token with authorization header

--- packages/client/src/app/graphql.module.ts
+++ packages/client/src/app/graphql.module.ts
@@ -1,12 +1,21 @@
-import {NgModule} from '@angular/core';
-import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
-import {HttpLink} from 'apollo-angular/http';
+import { NgModule } from '@angular/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, from, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { setContext } from '@apollo/client/link/context';
+import { HttpHeaders } from '@angular/common/http';
 
 const uri = 'http://localhost:8080/graphql'; // <-- add the URL of the GraphQL server here
-export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+export function createApollo(httpLink: HttpLink ): ApolloClientOptions<any> {
+  const accessToken = localStorage.getItem('accessToken');
+  const http = httpLink.create({ uri });
+  const setAuthorizationLink = setContext(() => ({
+    headers: new HttpHeaders().set(
+      'Authorization', `Bearer ${accessToken}`
+    )
+  }));
   return {
-    link: httpLink.create({uri}),
+    link: from([setAuthorizationLink, http]),
     cache: new InMemoryCache(),
   };
 }
@@ -20,4 +29,4 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
     },
   ],
 })
-export class GraphQLModule {}
+export class GraphQLModule { }
