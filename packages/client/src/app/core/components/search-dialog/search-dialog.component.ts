c9e9dbd1a8df3202599159d3214113b5f6f3b0f6
Ahmed B <techiediaries9@gmail.com>
2021-10-04 12:20:21 +0100

    Implement search dialog

--- packages/client/src/app/core/components/search-dialog/search-dialog.component.ts
+++ packages/client/src/app/core/components/search-dialog/search-dialog.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnInit,
+  OnDestroy
+} from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material/dialog';
+import {
+  User,
+  SearchUsersResponse,
+  UsersResponse
+} from 'src/app/shared';
 
 @Component({
   selector: 'app-search-dialog',
@@ -6,10 +22,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-dialog.component.css']
 })
 export class SearchDialogComponent implements OnInit {
-
-  constructor() { }
+  usersSubscription: Subscription | null = null;
+  users: User[] = [];
+  fetchMore: (feed: User[]) => void = () => { };
+  constructor(
+    public dialogRef: MatDialogRef<SearchDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public response: SearchUsersResponse,
+    public router: Router) { }
 
   ngOnInit(): void {
+    this.usersSubscription = this.response.data.subscribe({
+      next: (data: UsersResponse | null | undefined) => {
+        if (data?.searchUsers) {
+          this.users = data.searchUsers;
+        }
+      }
+    });
+    this.fetchMore = this.response.fetchMore;
+  }
+  ngOnDestroy(): void {
+    if (this.usersSubscription) {
+      this.usersSubscription.unsubscribe();
+    }
+  }
+  goToProfile(profileId: string): void {
+    this.router.navigateByUrl(`/users/profile/${profileId}`);
+  }
+  invokeFetchMore(): void {
+    this.fetchMore(this.users);
+  }
+  close(): void {
+    this.dialogRef.close();
   }
 
 }
+
+
+
+
