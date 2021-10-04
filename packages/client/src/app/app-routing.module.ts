bc40356042707eeb4bf23dde990614ec13ba501c
Ahmed B <techiediaries9@gmail.com>
2021-10-04 12:25:48 +0100

    Add auth guard to feed route(s)

--- packages/client/src/app/app-routing.module.ts
+++ packages/client/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { AuthGuard } from 'src/app/core';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'feed' },
   { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
-  { path: 'feed', loadChildren: () => import('./feed/feed.module').then(m => m.FeedModule) },
+  {
+    path: 'feed',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./feed/feed.module').then(m => m.FeedModule)
+  },
   { path: '**', component: PageNotFoundComponent }
 ];
 
@@ -14,3 +19,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
