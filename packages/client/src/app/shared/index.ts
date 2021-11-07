47929eae366f6b412676723d739c7d2c994652e5
Ahmed B <techiediaries9@gmail.com>
2021-11-07 11:26:31 +0100

    Add & export events types

--- packages/client/src/app/shared/index.ts
+++ packages/client/src/app/shared/index.ts
@@ -3,3 +3,5 @@ export * from './constants/user';
 export { User } from './models/user.model';
 export * from './types/user';
 export * from './types/auth';
+export { PostEvent } from './types/post.event';
+export { RemovePostEvent } from './types/removepost.event';
