a09dd6f3fdb9c1e818209af89ab8cb27b157c115
Ahmed B <techiediaries9@gmail.com>
2021-11-07 11:20:21 +0100

    Implement profile service

--- packages/client/src/app/users/services/profile/profile.service.ts
+++ packages/client/src/app/users/services/profile/profile.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import {
+  SetUserCoverGQL,
+  SetUserPhotoGQL,
+  SetUserBioGQL
+} from 'src/app/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  constructor(
+    private setUserBioGQL: SetUserBioGQL,
+    private setUserPhotoGQL: SetUserPhotoGQL,
+    private setUserCoverGQL: SetUserCoverGQL
+  ) { }
+  setUserBio(bio: string) {
+    return this.setUserBioGQL.mutate({
+      bio: bio
+    }).pipe(map(result => result.data!.setUserBio));
+  }
+  setUserPhoto(photoImage: File) {
+    return this.setUserPhotoGQL.mutate({
+      file: photoImage
+    },
+      {
+        context: {
+          useMultipart: true
+        }
+      }).pipe(map(result => result.data!.setUserPhoto));
+  }
+  setUserCover(coverImage: File) {
+    return this.setUserCoverGQL.mutate({
+      file: coverImage
+    },
+      {
+        context: {
+          useMultipart: true
+        }
+      }).pipe(map(result => result.data!.setUserCover));
+  }
 }
