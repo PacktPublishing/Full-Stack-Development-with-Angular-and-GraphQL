0aaf85e0fb56789e569c515c8fc5e684e3eeb16f
Ahmed B <techiediaries9@gmail.com>
2021-11-07 18:05:37 +0100

    Implement profile component

--- packages/client/src/app/core/index.ts
+++ packages/client/src/app/core/index.ts
@@ -6,3 +6,5 @@ export { AuthGuard } from './guards/auth/auth.guard';
 export * from './gql.services';
 export { BaseComponent }
     from './components/base.component';
+export { ProfileService }
+    from '../users/services/profile/profile.service';
