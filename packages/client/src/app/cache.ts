844939443a2f0bbcc68d8af0847c21bc03f1ba7d
Ahmed B <techiediaries9@gmail.com>
2021-11-07 18:15:04 +0100

    Define field policies

--- packages/client/src/app/cache.ts
+++ packages/client/src/app/cache.ts
@@ -11,6 +11,26 @@ export default new InMemoryCache({
                     }
                 }
             }
+        },
+        User: {
+            fields: {
+                coverImage: {
+                    read(coverImage) {
+                        return `url(${coverImage})`;
+                    }
+                },
+                createdAt: {
+                    read(createdAt) {
+                        return new Date(Number(createdAt))
+                            .toLocaleDateString('en-US', {
+                                weekday: 'short',
+                                day: 'numeric',
+                                year: 'numeric',
+                                month: 'long'
+                            });
+                    }
+                }
+            }
         }
     }
 });
