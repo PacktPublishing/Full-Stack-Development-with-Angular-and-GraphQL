5a704bbe4f8111cfe453d8f27c2bb48236853241
Ahmed B <techiediaries9@gmail.com>
2021-12-02 23:26:33 +0100

    Fix: add codegen generated files

--- packages/client/src/app/core/gql.services.ts
+++ packages/client/src/app/core/gql.services.ts
@@ -5,6 +5,72 @@ import * as Apollo from 'apollo-angular';
   @Injectable({
     providedIn: 'root'
   })
+  export class GetCommentsByPostIdGQL extends Apollo.Query<Operations.GetCommentsByPostIdQuery, Operations.GetCommentsByPostIdQueryVariables> {
+    document = Operations.GetCommentsByPostIdDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class CommentPostGQL extends Apollo.Mutation<Operations.CommentPostMutation, Operations.CommentPostMutationVariables> {
+    document = Operations.CommentPostDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class RemoveCommentGQL extends Apollo.Mutation<Operations.RemoveCommentMutation, Operations.RemoveCommentMutationVariables> {
+    document = Operations.RemoveCommentDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class GetLikesByPostIdGQL extends Apollo.Query<Operations.GetLikesByPostIdQuery, Operations.GetLikesByPostIdQueryVariables> {
+    document = Operations.GetLikesByPostIdDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class LikePostGQL extends Apollo.Mutation<Operations.LikePostMutation, Operations.LikePostMutationVariables> {
+    document = Operations.LikePostDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class RemoveLikeGQL extends Apollo.Mutation<Operations.RemoveLikeMutation, Operations.RemoveLikeMutationVariables> {
+    document = Operations.RemoveLikeDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
   export class UploadFileGQL extends Apollo.Mutation<Operations.UploadFileMutation, Operations.UploadFileMutationVariables> {
     document = Operations.UploadFileDocument;
     
@@ -29,6 +95,28 @@ import * as Apollo from 'apollo-angular';
   })
   export class RemovePostGQL extends Apollo.Mutation<Operations.RemovePostMutation, Operations.RemovePostMutationVariables> {
     document = Operations.RemovePostDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class GetPostsByUserIdGQL extends Apollo.Query<Operations.GetPostsByUserIdQuery, Operations.GetPostsByUserIdQueryVariables> {
+    document = Operations.GetPostsByUserIdDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class GetFeedGQL extends Apollo.Query<Operations.GetFeedQuery, Operations.GetFeedQueryVariables> {
+    document = Operations.GetFeedDocument;
     
     constructor(apollo: Apollo.Apollo) {
       super(apollo);
